d9deb7a0feab3137e77c15fe8021e55a172fbe06
Rachel Marvin Wan <121346040+RachelMarvinWan@users.noreply.github.com>
2024-12-11 17:36:40 +0800

    Update farmer-login.ts

--- api/auth/farmer-login.ts
+++ api/auth/farmer-login.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import bcrypt from 'bcrypt';
-import connectToDatabase from 'lib/mongodb.ts';
+import connectToDatabase from '@lib/mongodb';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
