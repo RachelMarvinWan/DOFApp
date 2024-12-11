f60e3fa4c8da689e753f73c0be24efbc357151d4
Rachel Marvin Wan <121346040+RachelMarvinWan@users.noreply.github.com>
2024-12-11 17:39:03 +0800

    Update submit.ts

--- api/income/submit.ts
+++ api/income/submit.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import connectToDatabase from 'lib/mongodb.ts';
+import connectToDatabase from '@/lib/mongodb';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
