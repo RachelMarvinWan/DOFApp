a84e52f5eeee9b668365058b27d1247e89d283ac
Rachel Marvin Wan <121346040+RachelMarvinWan@users.noreply.github.com>
2024-12-13 19:20:49 +0800

    Create farmer.ts

---
+++ models/farmer.ts
@@ -0,0 +1,12 @@
+import mongoose, {Schema, model, models } from 'mongoose'; 
+
+const farmerschema = new Schema({
+  email: { type: String, required: true, unique: true },
+  password: {type: String, required: true },
+  name: {type: String, required: true }, 
+  role: {type: String, required: true, unique: true }, 
+});
+
+const farmer = models.farmer || model('farmer', farmerschema);
+
+export default farmer; 
