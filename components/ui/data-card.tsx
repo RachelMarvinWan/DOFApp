20df003a12257674256b77c5fb92de4dc8960a84
Rachel <rachelmarvin2002@gmail.com>
2024-12-14 07:54:03 +0800

    Update data-card.tsx

--- components/ui/data-card.tsx
+++ components/ui/data-card.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { Card, CardContent, CardHeader, CardTitle } from 'components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import React from 'react'
 
 interface DataCardProps {
