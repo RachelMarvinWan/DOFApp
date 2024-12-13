9e55d4bd86d1a7d94b32adf2ba8665b0a65fbc9b
Rachel Marvin Wan <121346040+RachelMarvinWan@users.noreply.github.com>
2024-12-13 19:24:49 +0800

    Update farmer.ts

--- models/farmer.ts
+++ models/farmer.ts
@@ -3,7 +3,7 @@ import mongoose, {Schema, model, models } from 'mongoose';
 const farmerschema = new Schema({
   email: { type: String, required: true, unique: true },
   password: {type: String, required: true },
-  name: {type: String, required: true }, 
+  username: {type: String, required: true }, 
   role: {type: String, required: true, unique: true }, 
 });
 
