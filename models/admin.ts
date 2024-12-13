f73ee0b6455788d6443be8d9c403e2ebf71496a3
Rachel <rachelmarvin2002@gmail.com>
2024-12-13 20:50:09 +0800

    Update admin.ts

--- models/admin.ts
+++ models/admin.ts
@@ -5,6 +5,7 @@ const adminschema = new Schema ({
   password: {type: String, required: true },
   username: {type: String, required: true }, 
   role: {type: String, required: true },
+  organisation: {type: String },
 });
 
 const admin = models. admin || model('admin', adminschema); 
