57d0c31580cdf092c43af75c7d808202b580091b
Rachel <rachelmarvin2002@gmail.com>
2024-12-13 20:50:12 +0800

    Update farmer.ts

--- models/farmer.ts
+++ models/farmer.ts
@@ -5,6 +5,10 @@ const farmerschema = new Schema({
   password: {type: String, required: true },
   username: {type: String, required: true }, 
   role: {type: String, required: true, unique: true }, 
+  userID: { type: String, required: true, unique: true },
+  organisation: {type: String, required: true },
+  registered_by: {type: mongoose.Types.ObjectId,ref: 'admin', required: true },
+  created_at: { type: Date, default: Date.now },
 });
 
 const farmer = models.farmer || model('farmer', farmerschema);
