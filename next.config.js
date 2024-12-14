8cc270a7d86f35cd379ce71428cdeb4d19f2ff17
Rachel <rachelmarvin2002@gmail.com>
2024-12-14 08:47:39 +0800

    Update next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // ... other configurations ...
+  experimental: {
+    externalDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
+};
